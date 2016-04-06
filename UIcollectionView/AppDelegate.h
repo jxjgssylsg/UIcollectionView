10f7ef36ff96648071161630289a312eebe84a94
jxjgssylsg <jxjgssylsg@126.com>
2016-04-06 14:56:54 +0800

    first commit

---
+++ UIcollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIcollectionView
+//
+//  Created by mingdffe on 16/4/6.
+//  Copyright © 2016年 boxfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIcollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIcollectionView
+//
+//  Created by mingdffe on 16/4/6.
+//  Copyright © 2016年 boxfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
