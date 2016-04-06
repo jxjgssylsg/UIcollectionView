6301acabb5f3b0ebf3469095c0dbf8198234b040
jxjgssylsg <jxjgssylsg@126.com>
2016-04-06 16:23:13 +0800

    second commit basic operation

--- UIcollectionView/ViewController.m
+++ UIcollectionView/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
 
 @end
 
@@ -16,9 +16,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView *colView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    colView.backgroundColor = [UIColor grayColor];
+    [colView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"myCell"];
+    colView.delegate = (id)self;
+    colView.dataSource = (id)self;
+    //[colView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"myCell"];
+    
+    [self.view addSubview:colView];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (NSArray *)loadData {
+    NSArray *arr = [NSArray arrayWithObjects:@"cell", @"cell2", @"cell3", @"cell4", @"cell5", @"cell6", @"cell7",@"cell8",@"cell9",nil];
+    
+    return arr;
+}
+#pragma mark - UICollectionViewDatasource
+
+- (NSInteger) collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return [[self loadData] count];
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellID = @"myCell";
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellID forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor redColor];
+    return cell;
+}
+
+//配置item的size
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(nonnull UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(nonnull NSIndexPath *)indexPath
+{
+    return CGSizeMake(100, 60);
+}
+
+//配置item的边距
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(120, 80, 0, 0);
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
